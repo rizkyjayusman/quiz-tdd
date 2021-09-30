cac435c2044e0c4c06d2884f87e153634dbe8db6
rizky <rizky.jayusman96@gmail.com>
2021-09-30 07:36:01 +0700

    separate app runner and quiz app runner

--- src/main/java/com/cygnus/quiz/App.java
+++ src/main/java/com/cygnus/quiz/App.java
@@ -1,5 +1,6 @@
 package com.cygnus.quiz;
 
+import com.cygnus.quiz.app.QuizApp;
 import com.cygnus.quiz.entity.Choice;
 import com.cygnus.quiz.entity.Question;
 import com.cygnus.quiz.entity.Quiz;
@@ -13,41 +14,13 @@ public class App {
         Scanner scanner = new Scanner(System.in);
         boolean end = false;
         while(!end) {
-            Quiz quiz = new Quiz(3);
-            quiz.addQuestion(createQuestion("1 + 1 = ...", new Choice[] {new Choice("a", "5"), new Choice("b", "2")}, new Choice("b","2")));
-            quiz.addQuestion(createQuestion("3 + 2 = ...", new Choice[] {new Choice("a", "5"), new Choice("b", "4")}, new Choice("a","5")));
-            quiz.addQuestion(createQuestion("1 + 2 = ...", new Choice[] {new Choice("a", "5"), new Choice("b", "3")}, new Choice("b","3")));
-
-            for(int i = 0; i < 3; i++) {
-                Question question = quiz.peek();
-                System.out.println(question.getQuestion());
-                for(Choice choice: question.getChoiceList()) {
-                    System.out.println(choice.getChoice());
-                }
-                System.out.print("answer: ");
-                String input = scanner.nextLine();
-                quiz.answerQuestion(input);
-            }
-
-            System.out.println("Score: " + quiz.getCorrectAnswer() + "/" + quiz.getSize());
-
+            QuizApp.run();
             System.out.print("Try Again: ");
             String input = scanner.nextLine();
             if(input.equals("N") || input.equals("n")) {
                 end = true;
             }
         }
-    }
-
-    public static Question createQuestion(String questionStr, Choice[] choices, Choice answer) {
-        Question question = new Question();
-        question.setQuestion(questionStr);
-        for(Choice choice: choices) {
-            question.addChoice(choice);
-        }
-        question.setAnswer(answer);
-
-        return question;
     }
 
 }
